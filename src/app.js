8e52e328cf48f9cb62a229267df7c6031961f3db
ElizaValle <beteduvalle@gmail.com>
2023-09-06 10:25:51 -0300

    versão atualizada

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import { MongoClient } from "mongodb"
+import { MongoClient, ObjectId } from "mongodb"
 import joi from "joi"
 import { resolveSoa } from "dns"
 
